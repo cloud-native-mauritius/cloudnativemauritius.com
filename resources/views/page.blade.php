7568e367f2c30ce23f505f8247f5285491510088
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-08-15 00:16:28 +0400

    Page outline

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,7 +5,7 @@
 
         <h2 class="ml-6 text-md text-slate-700 font-bold uppercase">{{ $page->title }}</h2>
 
-        <div class="mt-4 bg-white lg:rounded-md lg:shadow-md p-4">
+        <div class="mt-4 bg-white lg:rounded-md lg:shadow-md p-4 lg:outline">
             <div class="prose max-w-full">
                 <div>{{ \Illuminate\Mail\Markdown::parse($page->content) }}</div>
             </div>
