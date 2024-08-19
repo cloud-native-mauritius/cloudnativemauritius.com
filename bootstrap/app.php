d0f9bf94307e2cfcc8a2c25ac147e98cbf808ed8
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-08-19 10:12:28 +0400

    Updated TrustProxies middleware as per Laravel docs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
