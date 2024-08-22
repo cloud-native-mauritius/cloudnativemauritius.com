7f266ae66e9a592bca6308257c2cef1c1957888d
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-08-22 21:25:07 +0400

    Improving the post header

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,17 +3,19 @@
 @section('content')
     <section class="mt-8 max-w-4xl mx-auto">
 
-        <h2 class="ml-6 text-md text-slate-700 font-bold uppercase">{{ $post->title }}</h2>
-
         <div class="mt-4 bg-white lg:rounded-md lg:shadow-md p-4 lg:outline">
+            @foreach ($post->categories as $category)
+                <span class="px-3 py-1 rounded-full bg-gray-800 text-white text-[11px] font-bold uppercase">{{ $category->name }}</span>
+            @endforeach
+            <h2 class="mt-1 mb-4 text-2xl font-bold">{{ $post->title }}</h2>
             <div class="mb-2">
                 @foreach ($post->authors as $author)
                 <div class="flex flex-row gap-2 items-center">
                     <div>
-                        <img class="h-10 rounded-full shadow-md" src="/storage/{{ $author->photo }}" />
+                        <img class="h-16 rounded-full shadow-md" src="/storage/{{ $author->photo }}" />
                     </div>
                     <div>
-                        <p class="text-xs uppercase font-semibold">By {{ $author->name }}</p>
+                        <p class="text-sm uppercase font-semibold">By {{ $author->name }}</p>
                         <p class="mt-0.5 text-xs font-light">{{ \Carbon\Carbon::parse($post->published_at)->format('d M Y') }}</p>
                     </div>
                 </div>
