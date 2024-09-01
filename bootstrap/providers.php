44caf52e3a942feb853ff057b8770ff3588780ab
Bruno Bernard <contact.brunobernard@gmail.com>
2024-09-01 21:02:54 +0400

    feat: add modified version of jetstream

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
 Route::get('/', EventController::class)->name('home');
 
 Route::get('/blog', BlogController::class)->name('blog');
