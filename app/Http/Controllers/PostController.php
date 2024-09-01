82413a97d2ed87f1e3c8df1f57184bb1e16ff1ba
Bruno Bernard <contact.brunobernard@gmail.com>
2024-09-01 20:29:43 +0400

    feat: prevent unpublished posts to be visible

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
     public function show(Post $post)
     {
+        abort_if(! $post->is_published, Response::HTTP_FORBIDDEN);
+
         $post->load(['authors', 'categories']);
 
         return view('post', compact('post'));
